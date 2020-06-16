4393764fc0c38e99f9b86a33b2ae5474f10182fb
Raudel Valdes <raudel.developer@gmail.com>
2020-06-16 19:43:54 -0400

    added encapsulation.

---
+++ Creating_Classes_TTV/MediaLibrary.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Creating_Classes_TTV
+{
+    class MediaLibrary
+    {
+        private MediaTypeBase[] _items;
+
+        public MediaLibrary(MediaTypeBase[] items)
+        {
+            _items = items;
+        }
+
+        public MediaTypeBase GetItemAt(int index)
+        {
+            if(index <= _items.Length - 1 && index >= 0)
+            {
+                return _items[index];
+            }
+
+            Console.WriteLine("An element at index " + index + " doesn't exist in the MediaLibrary.");
+
+            return null;
+        }
+    }
+}
--- Creating_Classes_TTV/Program.cs
+++ Creating_Classes_TTV/Program.cs
@@ -8,62 +8,107 @@ namespace Creating_Classes_TTV
         {
             try
             {
+                var MediaLibrary = new MediaLibrary(new MediaTypeBase[]
+                {
+                    new Movie("The Godfather", "Raudel Valdes"),
+                    new Book("Farenheit 451", "Camielle Riebel"),
+                    new Album("Rocket Man", "Adamary Valdes")
+                });
+
                 //Instantiating the classes
-                Movie firstMovie = new Movie("The Godfather", "Raudel Valdes");
- 
+                //Movie firstMovie = new Movie("The Godfather", "Raudel Valdes");
+
                 //firstMovie.Title = "The Godfather";
                 //firstMovie.Director = "Raudel Valdes";
-            
-                Book firstBook = new Book("Farenheit 451", "Camielle Riebel");
+
+                //Book firstBook = new Book("Farenheit 451", "Camielle Riebel");
 
                 //firstBook.Title = "Farenheit 451";
                 //firstBook.Author = "Camielle Riebel";
-            
-                Album firstAlbum = new Album("Rocket Man", "Adamary Valdes");
+
+                //Album firstAlbum = new Album("Rocket Man", "Adamary Valdes");
 
                 //firstAlbum.Title = "Rocket Man";
                 //firstAlbum.Artist = "Adamary Valdes";
 
-
-                Album SecondMovie = new Album("", "");
+                //SUPPOSED TO THROW AN EXCEPTION
+                //Album SecondMovie = new Album("", "");
 
                 //Console.WriteLine("Album: " + firstAlbum.Title + " by " + firstAlbum.Artist);
                 //Console.WriteLine("Book: " + firstBook.Title + " by " + firstBook.Author);
                 //Console.WriteLine("Movie: " + firstMovie.Title + " by " + firstMovie.Director);
 
 
-                Console.WriteLine(firstAlbum.GetDisplayText());
-                Console.WriteLine(firstBook.GetDisplayText());
-                Console.WriteLine(firstMovie.GetDisplayText());
+                DetectMediaType(MediaLibrary.GetItemAt(0));
+                DetectMediaType(MediaLibrary.GetItemAt(1));
+                DetectMediaType(MediaLibrary.GetItemAt(2));
 
-                firstBook.Loan("Raul Valdes");
-                Console.WriteLine(firstBook.GetDisplayText());
 
-                firstBook.Return();
-                Console.WriteLine(firstBook.GetDisplayText());
+                Display(MediaLibrary.GetItemAt(0));
+                Display(MediaLibrary.GetItemAt(1));
+                Display(MediaLibrary.GetItemAt(5));
 
-                firstMovie.Loan();
-                Console.WriteLine(firstMovie.GetDisplayText());
+                /*
+                    Console.WriteLine(firstAlbum.GetDisplayText());
+                    Console.WriteLine(firstBook.GetDisplayText());
+                    Console.WriteLine(firstMovie.GetDisplayText());
 
-                firstMovie.Return();
-                Console.WriteLine(firstMovie.GetDisplayText());
+                    firstBook.Loan("Raul Valdes");
+                    Console.WriteLine(firstBook.GetDisplayText());
 
-                DetectMediaType(firstAlbum);
-                DetectMediaType(firstMovie);
-                DetectMediaType(firstBook);
+                    firstBook.Return();
+                    Console.WriteLine(firstBook.GetDisplayText());
+
+                    firstMovie.Loan();
+                    Console.WriteLine(firstMovie.GetDisplayText());
+
+                    firstMovie.Return();
+                    Console.WriteLine(firstMovie.GetDisplayText());
+                */
 
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 Console.WriteLine("Exception: " + ex.Message);
             }
 
         }
 
-        public static void DetectMediaType(MediaTypeBase item)
+        public static void Display(MediaTypeBase item)
         {
 
+            if(item == null)
+            {
+                return;
+            }
+
+            if (item is Movie)
+            {
+                Console.WriteLine(((Movie)item).GetDisplayText());
+            }
+             else if (item is Book)
+            {
+                Console.WriteLine(((Book)item).GetDisplayText());
+            }
+            else if (item is Album)
+            {
+                Console.WriteLine(((Album)item).GetDisplayText());
+            }
+            else
+            {
+                throw new System.ArgumentException("Unexpected media subtype encountered.", "type");
+            }
+        }
+
+        public static void DetectMediaType(MediaTypeBase item)
+        {
             string type = "";
+
+            if (item == null)
+            {
+                return;
+            }
+
             if (item is Movie) type = "Movie";
             if (item is Book) type = "Book";
             if (item is Album) type = "Album";
@@ -73,7 +118,7 @@ namespace Creating_Classes_TTV
                 throw new System.ArgumentException("Unexpected media subtype encountered.", "type");
             }
 
-            Console.WriteLine(item.Title + " is " + type);
+           Console.WriteLine(item.Title + " is " + type);
         }
     }
 }
